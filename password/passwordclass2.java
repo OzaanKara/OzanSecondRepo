be38534872e9598bf273554fed6025d0d47144b6
OzaanKara <ozaankara67@gmail.com>
2022-07-24 03:34:09 -0400

    hey this is my message

---
+++ password/passwordclass2.java
@@ -0,0 +1,9 @@
+package password;
+
+public class passwordclass2 {
+    public static void main(String[] args) {
+       // eakjf;sj
+
+
+    }
+}
--- workforMac/thirdpractice.java
+++
@@ -1,7 +0,0 @@
-package workforMac;
-
-public class thirdpractice {
-    public static void main(String[] args) {
-        System.out.println("Test");
-    }
-}
